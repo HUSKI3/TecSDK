d30eb525443a293134ad4dd1d2fd6c49cc811e0b
HUSKI3 <ignispy@protonmail.com>
2022-06-29 13:11:34 +0200

    upd: remove docs for rich

--- tli.py
+++ tli.py
@@ -99,19 +99,3 @@ import time
 #        progress.update(task2, advance=0.3)
 #        progress.update(task3, advance=0.9)
 #        time.sleep(0.02)
-
-
-
-
-is_correct = False
-while not is_correct:
-    name = Prompt.ask("Enter your name", default="Martin")
-    is_correct = Confirm.ask(f"Is your name '{name}'?")
-
-age = Prompt.ask(
-    "What is your age group",
-    choices=["<18", "18-25", "25-35", ">35"],
-    default="25-35"
-)
-
-print(f"You are {name} and your age is {age}.")
